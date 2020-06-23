35eb73a64fead599fc82d6aa2054bd51214165be
nkdblack94 <nkdblack1294@gmail.com>
2020-06-23 22:06:00 +0700

    [Bài tập] Luyện tập sử dụng ArrayList, LinkedList trong Java Collection Framework

--- src/main/java/ProductManager.java
+++ src/main/java/ProductManager.java
@@ -1,124 +1,122 @@
 import java.util.*;
 
 public class ProductManager extends Product {
-   private List<Product> products = new ArrayList<Product>();
+    private List<Product> products = new ArrayList<Product>();
 
-   public void displayProductList (){
-       System.out.println("Product list");
-       for (int i = 0; i < products.size(); i++){
-           System.out.println("ID: " + products.get(i).getId() + " Product Name: " + products.get(i).getName() + " Price: " + products.get(i).getPrice());
-       }
-   }
+    public void displayProductList() {
+        System.out.println("Product list");
+        for (int i = 0; i < products.size(); i++) {
+            System.out.println("ID: " + products.get(i).getId() + " Product Name: " + products.get(i).getName() + " Price: " + products.get(i).getPrice());
+        }
+    }
 
-   public void addNewProduct (){
-       Scanner scanner = new Scanner(System.in);
-       System.out.println("Enter ID");
-       int id = scanner.nextInt();
-       System.out.println("Enter Product name");
-       String name = scanner.next();
-       System.out.println("Enter product price");
-       int price = scanner.nextInt();
-       Product product = new Product(id, name, price);
-       products.add(product);
-   }
+    public void addNewProduct() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter ID");
+        int id = scanner.nextInt();
+        System.out.println("Enter Product name");
+        String name = scanner.next();
+        System.out.println("Enter product price");
+        int price = scanner.nextInt();
+        Product product = new Product(id, name, price);
+        products.add(product);
+    }
 
-   public void editProduct (){
-       Scanner scanner = new Scanner(System.in);
-       System.out.println("Enter the id you want to edit");
-       int editId = scanner.nextInt();
-       checkEditId(editId);
-   }
+    public void editProduct() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the id you want to edit");
+        int editId = scanner.nextInt();
+        checkEditId(editId);
+    }
 
-   public void deleteProductId (){
-       Scanner scanner = new Scanner(System.in);
-       System.out.println("Enter the product id you want to delete");
-       int id = scanner.nextInt();
-       checkDeleteId(id);
-   }
+    public void deleteProductId() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the product id you want to delete");
+        int id = scanner.nextInt();
+        checkDeleteId(id);
+    }
 
-   public void searchProductName (){
-       Scanner scanner = new Scanner(System.in);
-       System.out.println("Enter the name of the product you want to search");
-       String name = scanner.nextLine();
-       checkName(name);
-   }
+    public void searchProductName() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the name of the product you want to search");
+        String name = scanner.nextLine();
+        checkName(name);
+    }
 
-   public void checkName (String name){
-       boolean check = true;
-       for (int i = 0; i < products.size(); i++){
-           if (products.get(i).getName().equals(name)){
-               System.out.println("ID: "+ products.get(i).getId() + " Name: " + products.get(i).getName() + " Price: " + products.get(i).getPrice());
-               check = false;
-           }
-       }
-       if (check){
-           throw new IndexOutOfBoundsException("Product name not found");
-       }
-   }
+    public void checkName(String name) {
+        boolean check = true;
+        for (int i = 0; i < products.size(); i++) {
+            if (products.get(i).getName().equals(name)) {
+                System.out.println("ID: " + products.get(i).getId() + " Name: " + products.get(i).getName() + " Price: " + products.get(i).getPrice());
+                check = false;
+            }
+        }
+        if (check) {
+            throw new IndexOutOfBoundsException("Product name not found");
+        }
+    }
 
-   public void checkEditId(int id){
-       boolean check = true;
-       Scanner scanner = new Scanner(System.in);
-       for (int i = 0; i < products.size(); i++){
-           if (id == products.get(i).getId()){
-               System.out.println("Enter edit product name");
-               String name = scanner.next();
-               products.get(i).setName(name);
-               System.out.println("Enter edit product price");
-               int price = scanner.nextInt();
-               products.get(i).setPrice(price);
-               check = false;
-           }
-       }
-       if (check){
-           throw new IndexOutOfBoundsException("id not found");
-       }
-   }
+    public void checkEditId(int id) {
+        boolean check = true;
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < products.size(); i++) {
+            if (id == products.get(i).getId()) {
+                System.out.println("Enter edit product name");
+                String name = scanner.next();
+                products.get(i).setName(name);
+                System.out.println("Enter edit product price");
+                int price = scanner.nextInt();
+                products.get(i).setPrice(price);
+                check = false;
+            }
+        }
+        if (check) {
+            throw new IndexOutOfBoundsException("id not found");
+        }
+    }
 
-    public void checkDeleteId(int id){
-       Product product = null;
-        for (int i = 0; i < products.size(); i++){
-            if (id == products.get(i).getId()){
+    public void checkDeleteId(int id) {
+        Product product = null;
+        for (int i = 0; i < products.size(); i++) {
+            if (id == products.get(i).getId()) {
                 product = products.get(i);
                 break;
             }
         }
-        if (product != null){
-           products.remove(product);
+        if (product != null) {
+            products.remove(product);
             System.out.println("delete successful");
         } else {
             throw new IndexOutOfBoundsException("id not found");
         }
     }
 
-    public void sortUpAscending (){
-       displayProductList();
-       Collections.sort(products, new Comparator<Product>() {
-           public int compare(Product o1, Product o2) {
-               int price1 = o1.getPrice();
-               int price2 = o2.getPrice();
-               if (price1 > price2){
-                   return 1;
-               } else if (price1 == price2) return 0;
-               else return -1;
-           }
-       });
-       displayProductList();
+    public void sortUpAscending() {
+        displayProductList();
+        Collections.sort(products, new Comparator<Product>() {
+            public int compare(Product o1, Product o2) {
+                int price1 = o1.getPrice();
+                int price2 = o2.getPrice();
+                if (price1 > price2) return 1;
+                else if (price1 == price2) return 0;
+                else return -1;
+            }
+        });
+        displayProductList();
     }
 
-    public void sortedDescending (){
-       displayProductList();
-       Collections.sort(products, new Comparator<Product>() {
-           public int compare(Product o1, Product o2) {
-               int price1 = o1.getPrice();
-               int price2 = o2.getPrice();
-               if (price1 < price2){
-                   return 1;
-               } else if (price1 == price2) return 0;
-               else return -1;
-           }
-       });
-       displayProductList();
+    public void sortedDescending() {
+        displayProductList();
+        Collections.sort(products, new Comparator<Product>() {
+            public int compare(Product o1, Product o2) {
+                int price1 = o1.getPrice();
+                int price2 = o2.getPrice();
+                if (price1 < price2) return 1;
+                else if (price1 == price2) return 0;
+                else return -1;
+            }
+        });
+        displayProductList();
     }
 
 }
